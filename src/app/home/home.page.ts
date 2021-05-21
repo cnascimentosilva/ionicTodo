036ccd85893e0934fb7eb745bc665d850229ab70
Técnico Informática <51121742019.1@senacrjedu.local>
2021-05-21 20:55:06 -0300

    atualizando a aplicação

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { AlertController, ToastController, ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,32 +8,93 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private alertControl: AlertController) {}
+  tasks: any[] = [];
+  constructor(private alertControl: AlertController,
+              private toastControl: ToastController,
+              private actionSheetControl: ActionSheetController,
+              ) {
+    let taskJSON = localStorage.getItem('taskDB');
 
-  async showAdd(){
+    if (taskJSON != null) {
+      this.tasks = JSON.parse(taskJSON);
+    }
+  }
+
+  async showAdd() {
     const alert = await this.alertControl.create({
       header: 'O que deseja fazer?',
-      inputs:[
-        {name: 'task',
-        type: 'text',
-        placeholder: 'Digite a nova tarefa',
+      inputs: [
+        {
+          name: 'task',
+          type: 'text',
+          placeholder: 'Digite a nova tarefa',
         }
       ],
       buttons: [
-        {text: 'cancelar',
-        role: 'cancel',
-        cssClass: 'secondary',
-        handler: () => {
-          console.log('cancelou');
+        {
+          text: 'cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('cancelou');
+          }
+        }, {
+          text: "adicionar",
+          handler: (form) => {
+            this.add(form.task);
+          }
         }
-      },{
-        text: "adicionar",
-        handler: (form) => {
-
-        }
-      }
       ]
-  });
-  await alert.present();
+    });
+    await alert.present();
+  }
+
+  async add(newTask: string) {
+    if (newTask.trim().length < 1) {
+      const toast = await this.toastControl.create({
+        message: 'Informe a tarefa!',
+        duration: 2000,
+        position: 'top',
+        color: 'danger'
+      });
+
+      toast.present();
+    }
+
+    let task = { name: newTask, done: false };
+    this.tasks.push(task)
+    this.updateLocalStorage();
+  }
+  updateLocalStorage() {
+    localStorage.setItem('taskDB', JSON.stringify(this.tasks));
+  }
+
+  async openAction(task: any) {
+    const actionSheet = await this.actionSheetControl.create({
+      header: 'O que fazer',
+      buttons: [
+        {
+          text: task.done ? 'desmarcar' : 'marcar', 
+          icon: task.done ? 'radio-button-off' : 'checkmark-circle',
+          handler: () => {
+            task.done = !task.done;
+            this.updateLocalStorage();
+          }
+        },{
+          text: 'cancelar',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancelar selecionado');
+          }
+        }
+
+      ]
+    });
+    await actionSheet.present();
+  }
+  deletar(task: any){
+    this.tasks = this.tasks.filter(taskArray => task != taskArray);
+    this.updateLocalStorage();
   }
 }
